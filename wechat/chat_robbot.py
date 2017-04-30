72e353ba45f48ce9c56947b5384f8e2bc895c1f3
RRRoger <chenpeng.0418@163.com>
2017-04-30 17:39:42 +0800

    微信自动回复 first commit

---
+++ wechat/chat_robbot.py
@@ -0,0 +1,78 @@
+# -*- encoding: utf-8 -*-
+import requests
+import itchat #这是一个用于微信回复的库
+import json
+ALLOW_GROUP = True  # 允许群聊
+ALLOW_SINGLE = True  # 允许私聊
+
+
+KEY = '8edce3ce905a4c1dbb965e6b35c3834d' #这个key可以直接拿来用
+ 
+# 向api发送请求
+def get_response(msg):
+  apiUrl = 'http://www.tuling123.com/openapi/api'
+  data = {
+    'key'  : KEY,
+    'info'  : msg,
+    'userid' : 'pth-robot',
+  }
+  try:
+    r = requests.post(apiUrl, data=data).json()
+    return r.get('text')
+  except:
+    return
+ 
+# 注册方法/群聊
+@itchat.msg_register(itchat.content.TEXT, isGroupChat=True)
+def tuling_reply_group(msg):
+  if not ALLOW_GROUP:
+    return 
+  # 为了保证在图灵Key出现问题的时候仍旧可以回复，这里设置一个默认回复
+  defaultReply = 'I received: ' + msg['Text']
+  # 如果图灵Key出现问题，那么reply将会是None
+
+  # 群里的show name
+  display_name = msg['User']['Self']['DisplayName']
+
+  reply = get_response(msg['Text'].replace(u'@%s' % display_name, ''))
+  # reply = get_response(msg['Text'])
+
+  # 群名称
+  group_name = msg['User']['NickName']
+
+  print u'私聊_msg from@ %s : [%s]' % (group_name, msg['Text'])
+  print u'私聊reply to@ %s : [%s]' % (group_name, reply)
+
+  if group_name in [
+    u'大家五一快乐！',
+    u'(1)(2)(3)(4)(5)(6)',
+    ]:
+    if msg['isAt']:
+
+
+      return u'@%s\u2005I : %s' % (msg['ActualNickName'], reply)
+    else:
+      return reply or defaultReply
+  else:
+    print u'私聊_msg from@ %s : [%s]' % (group_name, msg['Text'])
+
+# 注册方法/私聊
+@itchat.msg_register(itchat.content.TEXT)
+def tuling_reply_single(msg):
+  if not ALLOW_SINGLE:
+    return
+  # 为了保证在图灵Key出现问题的时候仍旧可以回复，这里设置一个默认回复
+  defaultReply = 'I received: ' + msg['Text']
+  # 如果图灵Key出现问题，那么reply将会是None
+  reply = get_response(msg['Text'])
+  print u'群聊msg from@ %s : [%s]' % (msg['User']['NickName'], msg['Text'])
+  print u'群聊reply to@ %s : [%s]' % (msg['User']['NickName'], reply)
+  # a or b的意思是，如果a有内容，那么返回a，否则返回b
+  return reply or defaultReply
+ 
+# 为了让修改程序不用多次扫码,使用热启动
+# 在auto_login()里面提供一个True，即hotReload=True
+# 即可保留登陆状态
+# 即使程序关闭，一定时间内重新开启也可以不用重新扫码
+itchat.auto_login(hotReload=True)
+itchat.run()
